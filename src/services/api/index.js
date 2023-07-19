66516ba9015c320ac6cdc69c12b7427e0a034400
clefersondannylo <clefersonnascimento8@gmail.com>
2023-07-19 20:00:02 -0300

    update api link

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://serverappmemprendedoras-production.up.railway.app",
+    baseURL: "https://serverappmemprendedoras-production.up.railway.app/api",
 
 })
 
