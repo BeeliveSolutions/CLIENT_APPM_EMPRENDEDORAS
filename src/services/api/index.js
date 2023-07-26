436e95919fc5c2a2e484893346a0e8267c8f1d93
Paulo Felipe <PauloFelipet15@gmail.com>
2023-07-26 15:38:01 -0300

    screen register has been created

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: "https://serverappmemprendedoras-production.up.railway.app/api",
 
+
+
 })
 
 
-export default api;
+export default api  
